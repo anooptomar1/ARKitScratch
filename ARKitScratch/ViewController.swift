b6a68a30242a565482be98585ce16e38da6cefbd
Yuya Horita <yuya@hyuya.local>
2017-12-02 16:38:54 +0900

    change project

--- ARKitScratch/ViewController.swift
+++ ARKitScratch/ViewController.swift
@@ -2,58 +2,24 @@
 //  ViewController.swift
 //  ARKitScratch
 //
-//  Created by Yuya Horita on 2017/11/25.
+//  Created by Yuya Horita on 2017/12/02.
 //  Copyright © 2017 Yuya Horita. All rights reserved.
 //
 
 import UIKit
-import SceneKit
-import ARKit
 
-final class ViewController: UIViewController, ARSCNViewDelegate {
-    @IBOutlet var sceneView: ARSCNView!
-    
+class ViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        sceneView.delegate = self
-        sceneView.scene = SCNScene()
-        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        let configuration = ARWorldTrackingConfiguration()
-        configuration.planeDetection = .horizontal
-        sceneView.session.run(configuration)
-        sceneView.showsStatistics = true
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        sceneView.session.pause()
-    }
-    
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-        let plane = SCNPlane(width: .init(planeAnchor.extent.x), height: .init(planeAnchor.extent.z))
-        let planeNode = SCNNode(geometry: plane)
-        planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
-        planeNode.eulerAngles.x = -.pi / 2
-        planeNode.opacity = 0.25
-        
-        node.addChildNode(planeNode)
-    }
-    
-    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        guard let planeAnchor = anchor as? ARPlaneAnchor,
-            let planeNode = node.childNodes.first,
-            let plane = planeNode.geometry as? SCNPlane else { return }
-        
-        planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
-        plane.width = .init(planeAnchor.extent.x)
-        plane.height = .init(planeAnchor.extent.z)
-    }
+
+
 }
+
