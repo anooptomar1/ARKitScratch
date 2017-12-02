0f6e509c0923ab0021c819ffa161b38dae06ddab
Yuya Horita <yuya@hyuya.local>
2017-12-02 18:07:54 +0900

    arkit with single view applicaton

--- ARKitScratch/ViewController.swift
+++ ARKitScratch/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    @IBOutlet fileprivate weak var arView: ARSCNView!
+    fileprivate let virtualObject: SCNNode = .init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configure()
+        configureVirtualObject()
+        configureArSession()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        arView.session.pause()
     }
-
-
 }
 
+private extension ViewController {
+    func configure() {
+        let tap = UITapGestureRecognizer()
+        arView.addGestureRecognizer(tap)
+    }
+    
+    func configureVirtualObject() {
+        let scene = SCNScene(named: "art.scnassets/engel-1/engel.scn")!
+        scene.rootNode.childNodes.forEach { virtualObject.addChildNode($0) }
+    }
+    
+    func configureArSession() {
+        arView.scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        arView.delegate = self
+        arView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        arView.session.run(configuration)
+    }
+}
+
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        let geometry = SCNPlane(width: .init(planeAnchor.extent.x), height: .init(planeAnchor.extent.z))
+        geometry.materials.first?.diffuse.contents = UIColor.blue
+        
+        let planeNode = SCNNode(geometry: geometry)
+        planeNode.transform = SCNMatrix4MakeRotation(-.pi / 2, 1, 0, 0)
+        
+        DispatchQueue.main.async {
+            node.addChildNode(planeNode)
+            node.addChildNode(self.virtualObject)
+        }
+    }
+}
+
+extension ViewController: ARSessionDelegate {
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        
+    }
+}
